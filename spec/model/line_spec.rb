eb5d5a1cbc8c31b02c306013de9f1755a4ca5b85
Mattias Bergbom <mattias@swarmplanet.com>
2014-12-08 21:12:40 +0100

    New :expect syntax across the board

--- spec/model/line_spec.rb
+++ spec/model/line_spec.rb
@@ -4,21 +4,21 @@ describe Line do
   describe "#number" do
     it "returns the number it was given" do
       line = Line.new 2, 4, "abcdef"
-      line.number.should == 2
+      expect(line.number).to eq(2)
     end
   end
 
   describe "#count" do
     it "returns the count it was given" do
       line = Line.new 2, 4, "abcdef"
-      line.count.should == 4
+      expect(line.count).to eq(4)
     end
   end
 
   describe "#text" do
     it "returns the text it was given" do
       line = Line.new 2, 4, "abcdef"
-      line.text.should == "abcdef"
+      expect(line.text).to eq("abcdef")
     end
   end
 
--- spec/model/project_spec.rb
+++ spec/model/project_spec.rb
@@ -4,14 +4,14 @@ describe Project do
   describe "#files" do
     it "returns no files if it is empty" do
       project = Project.new
-      project.files.count.should == 0
+      expect(project.files.count).to eq(0)
     end
 
     it "returns the files it has been given" do
       project = Project.new
       project << File.new("foobar.cpp")
       project << File.new("boofar.cpp")
-      project.files.count.should == 2
+      expect(project.files.count).to eq(2)
       expect(project.files[0].name).to eq("foobar.cpp")
       expect(project.files[1].name).to eq("boofar.cpp")
     end
