6008427070c571c671792fc712e7b339bfc83225
Mattias Bergbom <mattias@swarmplanet.com>
2014-12-17 08:46:05 +0100

    Add JSON support

---
+++ lib/json_formatter.rb
@@ -0,0 +1,43 @@
+
+require_relative './project'
+require_relative './file'
+require_relative './line'
+
+require 'json'
+
+module GCOV
+  
+  class JSONFormatter
+    
+    def initialize project, va={}
+      @project = project
+
+      @json = {
+        'files' => []
+      }
+
+      @project.files.each do |file|
+        @json['files'] << { 
+          'name' => file.name,
+          'meta' => file.meta,
+          'stats' => file.stats,
+          'lines' => []
+        }
+
+        file.lines.select{|line| line.number > 0}.each do |line|
+          @json['files'][-1]['lines'] << { 
+            'number' => line.number,
+            'count' => line.count,
+            'text' => line.text
+          } 
+        end # each line
+      end # each file
+    end # initialize
+    
+    def print
+      puts @json.to_json
+    end # JSONFormatter#print
+
+  end # JSONFormatter
+
+end # GCOV
