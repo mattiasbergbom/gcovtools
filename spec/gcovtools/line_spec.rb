3a7f07be751b75937480717cc14b6ff222372b02
Mattias Bergbom <mattias@swarmplanet.com>
2015-03-06 14:06:05 +0100

    Change spec wording to 'should'. Tweak Line spec.

--- spec/gcovtools/line_spec.rb
+++ spec/gcovtools/line_spec.rb
@@ -2,46 +2,46 @@ require_relative '../spec_helper'
 
 describe GCOVTOOLS::Line do
   describe "#number" do
-    it "returns the number it was given" do
+    it "should return the number it was given" do
       line = GCOVTOOLS::Line.new 2, 4, "abcdef"
       expect(line.number).to eq(2)
     end
   end
 
   describe "#count" do
-    it "returns the count it was given" do
+    it "should return the count it was given" do
       line = GCOVTOOLS::Line.new 2, 4, "abcdef"
       expect(line.count).to eq(4)
     end
   end
 
   describe "#text" do
-    it "returns the text it was given" do
+    it "should return the text it was given" do
       line = GCOVTOOLS::Line.new 2, 4, "abcdef"
       expect(line.text).to eq("abcdef")
     end
   end
 
   describe ".parse" do
-    it "parses gcov format line with count" do
+    it "should parse gcov format line with count" do
       line = GCOVTOOLS::Line.parse "        2:   55:  std::string StringUtil::ltrim( const std::string &s ) "
       expect(line.number).to eq(55)
       expect(line.count).to eq(2)
       expect(line.text).to eq("  std::string StringUtil::ltrim( const std::string &s ) ")
     end
 
-    it "parses empty gcov format line" do
+    it "should parse empty gcov format line" do
       line = GCOVTOOLS::Line.parse "        -:   35:   * don't worry about excessive copying."
       expect(line.number).to eq(35)
       expect(line.count).to eq(:none)
       expect(line.text).to eq("   * don't worry about excessive copying.")
     end
 
-    it "parses missed gcov format line" do
-      line = GCOVTOOLS::Line.parse "    #####:   89:  }  "
+    it "should parse missed gcov format line" do
+      line = GCOVTOOLS::Line.parse "    #####:   89:  int somestatement = /*ignored value*/ xyz; }  "
       expect(line.number).to eq(89)
       expect(line.count).to eq(:missed)
-      expect(line.text).to eq("  }  ")
+      expect(line.text).to eq("  int somestatement = /*ignored value*/ xyz; }  ")
     end
 
   end
